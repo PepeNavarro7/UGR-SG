9cc563f5fbdc6e83ec5f734b33f3c650db47ea21
PepeNavarro7 <navarrocuartero7@gmail.com>
2023-03-09 15:25:33 +0100

    Cono funcionando

--- Practica 1/ejerciciosThree/Ejercicios/2. Geometria Basica 3D/Cono.js
+++ Practica 1/ejerciciosThree/Ejercicios/2. Geometria Basica 3D/Cono.js
@@ -9,16 +9,19 @@ class Cono extends THREE.Object3D {
     this.createGUI(gui,titleGui);
     
     // Un Mesh se compone de geometría y material
-    let geometry = new THREE.ConeGeometry(1,1,3); //radio, altura y devisiones
+    this.geometry = new THREE.ConeGeometry(1,1,3); //radio, altura y divisiones
     // Como material se crea uno a partir de las normales
-    let material = new THREE.MeshNormalMaterial();
+    this.material = new THREE.MeshNormalMaterial();
 
     // Ya podemos construir el Mesh
-    let modelo = new THREE.Mesh (geometry, material);
+    this.modelo = new THREE.Mesh (this.geometry, this.material);
     // Y añadirlo como hijo del Object3D (el this)
-    this.add (modelo);
+    this.add (this.modelo);
 
     this.rotacion = 0.0;
+    this.radio = 1.0;
+    this.altura = 1.0;
+    this.resolucion = 3;
   }
   
   createGUI (gui,titleGui) {
@@ -26,7 +29,7 @@ class Cono extends THREE.Object3D {
     this.guiControls = {
       radio : 1.0,
       altura : 1.0,
-      resolucion : 3,
+      resolucion : 4,
     } 
     
     // Se crea una sección para los controles de la caja
@@ -35,13 +38,22 @@ class Cono extends THREE.Object3D {
     // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
     folder.add (this.guiControls, 'radio', 0.1, 5.0, 0.1).name ('Radio: ').listen();
     folder.add (this.guiControls, 'altura', 0.1, 5.0, 0.1).name ('Altura: ').listen();
-    folder.add (this.guiControls, 'resolucion', 3, 60, 1).name ('Resolucion: ').listen();
+    folder.add (this.guiControls, 'resolucion', 3, 10, 1).name ('Resolucion: ').listen();
   }
   
   update () {
-    //this.rotacion+=0.01;
+    this.rotacion+=0.01;
     this.rotation.set (this.rotacion,this.rotacion,this.rotacion);
-    this.scale.set (this.guiControls.radio,this.guiControls.altura,this.guiControls.resolucion);
+    if(this.guiControls.radio != this.radio || this.guiControls.altura != this.altura || this.guiControls.resolucion != this.resolucion){
+      this.radio = this.guiControls.radio;
+      this.altura = this.guiControls.altura;
+      this.resolucion = this.guiControls.resolucion;
+      this.geometry.dispose(); // esto parece que no funciona
+      this.remove(this.modelo); // esto elimina el modelo de la escena
+      this.geometry = new THREE.ConeGeometry(this.radio,this.altura,this.resolucion);
+      this.modelo = new THREE.Mesh (this.geometry, this.material);
+      this.add(this.modelo);
+    }
   }
 }
 
