525bb8467bce2ad723ca34a3d512d055d350c201
PepeNavarro7 <navarrocuartero7@gmail.com>
2023-05-23 19:15:36 +0200

    Empiezo con la maceta

---
+++ Practica 2/Maceta/Maceta.js
@@ -0,0 +1,9 @@
+import * as THREE from '../libs/three.module.js'
+import * as TWEEN from '../libs/tween.esm.js'
+import { CSG } from '../libs/CSG-v2.js'
+ 
+class Maceta extends THREE.Object3D {
+
+}
+
+export { Maceta };
